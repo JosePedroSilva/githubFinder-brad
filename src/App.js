15fca6797a053fc5da6feb200764f85a35bb7aa5
JosePedro <josepedro88@live.com.pt>
2020-07-21 16:07:30 +0100

    components and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
 	render() {
-		const name = 'Joe';
-		const loading = false;
-
-		return <div className="App">{loading ? <h4>Loading...</h4> : <h1>React app from {name}</h1>}</div>;
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
 	}
 }
 
