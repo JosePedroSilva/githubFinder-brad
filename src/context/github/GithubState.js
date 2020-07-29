9c91595b1f5a64e343f2ac54a76eed653395a1a9
JosePedro <josepedro88@live.com.pt>
2020-07-29 15:32:55 +0100

    search_users refactor to include context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
 import GitHubReducer from './githubReducer';
-import { SEARCH_USER, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
+import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
 const GithubState = props => {
 	const initialState = {
@@ -14,13 +14,29 @@ const GithubState = props => {
 
 	const [ state, dispatch ] = useReducer(GitHubReducer, initialState);
 
+	const searchUsers = async text => {
+		setLoading();
+
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({
+			type: SEARCH_USERS,
+			payload: res.data.items
+		});
+	};
+
+	const setLoading = () => dispatch({ type: SET_LOADING });
+
 	return (
 		<GithubContext.Provider
 			value={{
 				users: state.users,
 				user: state.user,
 				repos: state.repos,
-				loading: state.loading
+				loading: state.loading,
+				searchUsers
 			}}
 		>
 			{props.children}
