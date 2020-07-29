003e2b74e7a96b94edd8155f1b5d7c6f5e34cad0
JosePedro <josepedro88@live.com.pt>
2020-07-29 15:59:03 +0100

    single user page refactor

--- src/App.js
+++ src/App.js
@@ -11,22 +11,9 @@ import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-	const [ users, setUsers ] = useState([]);
-	const [ user, setUser ] = useState({});
 	const [ repos, setRepos ] = useState([]);
 	const [ loading, setLoading ] = useState(false);
 	const [ alert, setAlert ] = useState(null);
-
-	const getUser = async username => {
-		setLoading(true);
-
-		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		setUser(res.data);
-		setLoading(false);
-	};
 
 	const getUserRepos = async username => {
 		setLoading(true);
@@ -66,16 +53,7 @@ const App = () => {
 							<Route
 								exact
 								path="/user/:login"
-								render={props => (
-									<User
-										{...props}
-										getUser={getUser}
-										getUserRepos={getUserRepos}
-										user={user}
-										repos={repos}
-										loading={loading}
-									/>
-								)}
+								render={props => <User {...props} getUserRepos={getUserRepos} repos={repos} />}
 							/>
 						</Switch>
 					</div>
